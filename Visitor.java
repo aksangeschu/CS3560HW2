4915ca2fedf045b8eadf11bc5d292677a0daa59a
Ayanna Sanges-Chu <89478813+aksangeschu@users.noreply.github.com>
2024-06-21 20:24:18 -0700

    Delete Visitor.java

--- Visitor.java
+++
@@ -1,8 +0,0 @@
-/**
- * AnalysisVisitor interface for the Visitor pattern.
- * Defines methods for visiting Users and UserGroups.
- */
-public interface Visitor {
-    void visit(User user);
-    void visit(UserGroup userGroup);
-}
